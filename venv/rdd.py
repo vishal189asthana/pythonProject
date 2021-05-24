db977b9aed3ab5f6f4d0c0d9d85200d004bd79e7
Vishal Asthana <vishal.asthana@brillio.com>
2021-05-24 13:02:32 +0530

    added rdd name

--- venv/rdd.py
+++ venv/rdd.py
@@ -1,5 +1,7 @@
 from pyspark import SparkContext
 sc = SparkContext("local", "count app")
+
+sc.setLogLevel("WARN")
 # words = sc.parallelize (
 #     ["scala",
 #      "java",
@@ -34,14 +36,31 @@ fore = words.foreach(f)
 # filtered = words_filter.collect()
 # print ("Fitered RDD -> %s" % (filtered))
 
-words_filter=words.filter((lambda x:'py' in x)).collect()
-print ('filter %s is',words_filter)
-
-
-words_filter=words.filter(lambda  x:'spark' in x).collect()
-print(words_filter)
+# words_filter=words.filter((lambda x:'py' in x)).collect()
+# print ('filter %s is',words_filter)
+#
+#
+# words_filter=words.filter(lambda  x:'spark' in x).collect()
+# print(words_filter)
 
 
 words_map = words.map(lambda x: (x, 1))
 mapping = words_map.collect()
 print "Key value pair -> %s" % (mapping)
+
+
+wordsmap=words.map(lambda x:x.upper()).collect()
+print (wordsmap)
+
+from operator import sub
+
+nums = sc.parallelize([1, 2, 3, 4, 5])
+adding = nums.reduce(sub)
+print "Adding all the elements -> %i" % (adding)
+
+x = sc.parallelize([("spark", 1), ("hadoop", 4)])
+y = sc.parallelize([("spark", 2), ("hadoop", 5)])
+joined = x.join(y)
+final = joined.collect()
+
+print ("Join RDD -> %s" % (final))
